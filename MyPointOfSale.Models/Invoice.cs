cf7206f44e0fff757ccb9951b30b9b097aaa9e5d
franpaulinoinfante <franpaulinoinfan@gmail.com>
2025-04-07 22:20:17 -0400

    update

--- MyPointOfSale.Models/Invoice.cs
+++ MyPointOfSale.Models/Invoice.cs
@@ -1,31 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyPointOfSale.Models
 {
-    public class Invoice 
+    public class Invoice
     {
         public int InvoiceId { get; set; }
+        public User User { get; set; }
         public Client Client { get; set; }
+        public string NCF { get; set; }
         public DateTime CreateAt { get; set; }
         public decimal Total { get; set; }
         public decimal ITBIS { get; set; }
-        public User User { get; set; }
-        public string NCF { get; set; }
         public List<InvoiceLine> InvoiceLines { get; set; } = new List<InvoiceLine>();
     }
 }
-
-/*
- [InvoiceId]
-      ,[CreateAt]
-      ,[ClientId]
-      ,[Total]
-      ,[ITBIS]
-      ,[UserId]
-      ,[NCF]
-
-*/
